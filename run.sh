cdb351b446b0d986d77f9bcb58a45de06fb2c3d6
Michael Peter Christen <mc@yacy.net>
2024-11-24 11:50:25 +0100

    more robust start script

--- run.sh
+++ run.sh
@@ -4,22 +4,17 @@
 cd "$(dirname "$0")"
 
 # check the local environment for python3
-python3 --version
-pip3 --version
+python3.12 --version
+pip3.12 --version
 
 # Check if virtual environment exists
 if [ ! -d ".venv" ]; then
-    python3 -m venv .venv
-    source .venv/bin/activate
-    pip3 install --upgrade pip
-    pip3 install -r requirements.txt
-else
-    source .venv/bin/activate
-    # Check if requirements are already installed
-    if ! pip3 check > /dev/null 2>&1; then
-        pip3 install --upgrade pip
-        pip3 install -r requirements.txt
-    fi
+    python3.12 -m venv .venv
 fi
 
-python3 server.py --quantize 8 --host 0.0.0.0
+# always look for upgrades
+source .venv/bin/activate
+pip3.12 install --upgrade pip
+pip3.12 install --upgrade -r requirements.txt
+
+python3.12 server.py --quantize 8 --host 0.0.0.0
